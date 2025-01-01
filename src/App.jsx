68501c76f8d0ad796ba840ee29c2b215968bf5f7
hemanthdev <hemanth.dev.fe@gmail.com>
2025-01-01 23:58:04 +0530

    a1

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,45 @@
-import Theme from "./Theme_Components/Theme"
-import "./App.css"
-import { useDispatch, useSelector } from "react-redux"
-import {toggleTheme} from "./reducers/ThemeSlice"
- import HomePage from "./cricketGame-HomePage/HomePage-cricket"
-// import MemoryGame from "./memoryGame_Component/memoryGame"
-import AuthPage from "./Form_Component/form"
+import { createContext, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleTheme } from "./reducers/ThemeSlice";
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Tile from "./Tile_Component/tile"
-import Quiz from "./quizGame_Component/quizGame"
 
+import Theme from "./Theme_Components/Theme";
+import "./App.css";
+import HomePage from "./cricketGame-HomePage/HomePage-cricket";
+import AuthPage from "./Form_Component/form";
+import Tile from "./Tile_Component/tile";
+import Quiz from "./quizGame_Component/quizGame";
+import TextAnimation from "./TicTocToe_component/TextAnimation";
 
-//  import MemoryGame from "./memoryGame_Component/memoryGame"
-// import Game from "./TicTocToe_component/game";
- import TextAnimation from "./TicTocToe_component/TextAnimation"
-import { createContext, useState } from "react"
-// import Tile from "./Tile_Component/tile"
-// import AuthPage from "./dummy-practice/practiceone"
- //import RegistrationForm from "./dummy-practice/practiceone"
-// import Quiz from "./quizGame_Component/quizGame"
-// eslint-disable-next-line react-refresh/only-export-components
-export let context =createContext()
-function App()
-{
-    const darkMode=useSelector((state)=>state.Theme.DarkMode)
-     const dispatch=useDispatch()
-    const handleTheme=()=>{
-       dispatch(toggleTheme())
-    }
+export let context = createContext();
+
+function App() {
+    const darkMode = useSelector((state) => state.Theme.DarkMode);
+    const dispatch = useDispatch();
+    const handleTheme = () => {
+        dispatch(toggleTheme());
+    };
     const winner = useSelector((state) => state.game.winner);
-     const [isAuthenticated, setIsAuthenticated] = useState(false);
-    return(
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    return (
         <>
-       <context.Provider value={winner}>
-       <div className={darkMode?"App dark":"App light"}>
-        <Theme click={handleTheme} />
-        {/* <div className="center-content"> <Game /> </div> */}
-        {/* <TextAnimation/> */}
-        
-        {/* <Quiz/> */}
-        {/* <MemoryGame/> */}
-        {/* <HomePage/> */}
-        {/* <Tile/> */}
-        {/* <AuthPage/> */}
-        {/* <RegistrationForm/> */}
-        
-        <Router>
-            <Routes>
-            <Route path="/" element={<AuthPage setAuth={setIsAuthenticated} />} />
+            <context.Provider value={winner}>
+                <div className={darkMode ? "App dark" : "App light"}>
+                    <Theme click={handleTheme} />
+                    <Router>
+                        <Routes>
+                            <Route path="/" element={<AuthPage setAuth={setIsAuthenticated} />} />
                             <Route path="/tile" element={isAuthenticated ? <Tile /> : <Navigate to="/" />} />
                             <Route path="/quiz" element={isAuthenticated ? <Quiz /> : <Navigate to="/" />} />
                             <Route path="/memory" element={isAuthenticated ? <TextAnimation /> : <Navigate to="/" />} />
                             <Route path="/cricket" element={isAuthenticated ? <HomePage /> : <Navigate to="/" />} />
-            </Routes>
-        </Router>
-        </div>
-       </context.Provider>
-       
+                        </Routes>
+                    </Router>
+                </div>
+            </context.Provider>
         </>
-    )
+    );
 }
-export default App
+
+export default App;
