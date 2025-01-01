4ce1079d67c62e017cf875a967872ac53dd55b67
hemanthdev <hemanth.dev.fe@gmail.com>
2025-01-01 10:46:18 +0530

    12

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vite.dev/config/
+// Define __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      'use-sync-external-store-with-selector': path.resolve(__dirname, 'node_modules/use-sync-external-store-with-selector/cjs/use-sync-external-store-with-selector.production.js')
+    }
+  }
+});
